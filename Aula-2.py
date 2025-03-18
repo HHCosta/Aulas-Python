431e2c3cb6b816e37f690ca993ca778474eaed11
HHCosta <heitorhayato@gmail.com>
2025-03-17 22:47:11 -0300

    Aula1

--- Aula-2.py
+++ Aula-2.py
@@ -1,2 +1,27 @@
 print("Hello World!")
 
+#Exercicio 1
+print("Heitor\n")
+
+#Exercicio 2
+a = 3
+b = 5
+c = 2 * a
+d = 3 * b
+print(c*d)
+
+#Exercicio 3
+# print(a+b+c)
+#
+e = float(input("Numero 1 ="))
+f = float(input("Numero 2 ="))
+g = float(input("Numero 3 ="))
+soma =e+f+g
+print(f"Soma = {soma:.2f}")
+
+# h = 24/13
+# print(f"O número com duas casas é {h:.2f}\n")
+#
+# numero = float(round(3.3224, 3))
+# print('O número com tres casas decimais é',numero,'.')
+
