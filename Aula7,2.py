0080c0517620fe5ff24eadce9e5335cbbc447dd9
labsfiap <heitorhayato@gmail.com>
2025-10-20 22:37:21 -0300

    Aula7

---
+++ Aula7,2.py
@@ -0,0 +1,9 @@
+# from Aula7 import *
+#
+# a = float(input("numero 1 ="))
+# b = float(input("numero 2 ="))
+# print(f"Soma = {soma(a,b)}")
+# print(f"multiplicação = {multiplicacao(a,b)}")
+# print(f"subtração = {subtracao(a,b)}")
+# print(f"divisão = {divisao(a,b)}")
+
---
+++ Aula7.py
@@ -0,0 +1,62 @@
+def multiplicacao(a, b):
+    return a*b
+
+def soma(a, b):
+    return a+b
+
+def subtracao(a, b):
+    return a-b
+
+def divisao(a, b):
+    return a/b
+
+def ehpar(x):
+ return x % 2 == 0
+def par_ou_impar(x):
+ if ehpar(x):
+    return "par"
+ else:
+    return "ímpar"
+
+# c = int(input("numero 1"))
+# d = int(input("numero 2"))
+#
+# def maximo(a,b):
+#     if a < b:
+#         return b
+#     else:
+#         return a
+# print(maximo(c,d))
+#
+#
+# c = int(input("numero 1"))
+# d = int(input("numero 2"))
+#
+# def multiplos(a,b):
+#     if a%b == 0:
+#         return True
+#     else:
+#         return False
+# print(multiplos(c,d))
+
+# def areaQ(a):
+#     return a*a
+#
+# b = int(input("lado"))
+#
+# print(areaQ(b))
+
+# def areaT(a,b):
+#     return (a*b)/2
+#
+# c = int(input("lado"))
+# d = int(input("altura"))
+# print(areaT(c, d))
+
+# def linha(a):
+#     return "_" * a
+#
+# b = int(input("tamanho da linha"))
+# print(linha(b))
+
+
